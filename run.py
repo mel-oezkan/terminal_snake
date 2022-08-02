49b81812a20f39576724377c09d173e918b3163d
moezkan <m.oezkan@gerhard-schubert.de>
2022-08-02 19:53:29 +0200

    added platform checker

--- run.py
+++ run.py
@@ -1,5 +1,20 @@
-import curses
+
+import sys
 from window import Window
+import os
+import platform
+
+
+# curses does not run out the box in windows and some
+# additional packages have to be installed
+if platform.system() == 'win32':
+    try:
+        import curses
+    except Exception as e:
+        os.system("pip install windows-curses")
+        import curses
+
+import curses
 
 
 if __name__ == "__main__":
